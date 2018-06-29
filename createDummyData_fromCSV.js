f69ba8510a96c505b8b2e012006b0d7650706f0d
ajshiff <ajshiffler@gmail.com>
2018-06-28 21:10:42 -0600

    The majority of the program is done. There is just a few user-variable-checks that need to be done to aid in trouble-shooting and error-handling

--- createDummyData_fromCSV.js
+++ createDummyData_fromCSV.js
@@ -9,7 +9,44 @@
 // DECLARE DEPENDENCIES AND CONSTANTS
 const fs = require('fs');
 const d3 = require('d3-dsv');
-const dsv_location = 'C:\\Users\\ajshiff\\Documents\\git\\d3-countries-map\\src\\copy-of-countries.csv';
+// const dsv_location = 'C:\\Users\\ajshiff\\Documents\\git\\d3-countries-map\\src\\copy-of-countries.csv';
+const dsv_location = process.argv[2];
+var outputLocation = process.argv[3];
+var outputName = process.argv[4];
+
+/********************************************************************
+ * 
+ ********************************************************************/
+const checkUserVariables = function () {
+    let err;
+    let doNotContinue = false;
+    // console.log(fs.accessSync(dsv_location));
+    // CHECK ARGUEMENT 1
+    if (!fs.existsSync(dsv_location)) {
+        err = 'That File Might Not Exist!';
+        doNotContinue = true
+    } 
+    else if (dsv_location.slice(-4) !== '.csv') {
+        err = 'This Program Requires a .csv File-Type!';
+        doNotContinue = true
+    }
+    if(doNotContinue){process.exit(console.log(err));}
+
+    // CHECK ARGUEMENT 2
+    if(!process.argv[3]) {outputLocation = './'}
+    // console.log(outputLocation.slice(-1));
+    // if(outputLocation.slice(-1) !== '/' || outputLocation.slice(-1) !== '\\'){
+    //     err = 'It looks like your output location is not a directory. Try Again.';
+    //     doNotContinue = true;
+    // }
+    if(doNotContinue){process.exit(console.log(err));}
+    // CHECK ARGUEMENT 3
+    if(!process.argv[4]) {outputName = 'new.csv'}
+    if(outputName.slice(-4) !== '.csv'){outputName += '.csv';}
+    
+    // console.log(dsv_location.slice(-4));
+    // console.log(dsv_location + '\n' + outputLocation + '\n' + outputName + '\n');
+}
 
 /********************************************************************
  * conversionSeries() takes a d3-dsv input variable and runs it
@@ -59,7 +96,7 @@ const createCSV = function (dummyData) {
             });
         output = output.slice(0,-1);
     });
-    console.log(output);
+    // console.log(output);
     return output;
 }
 
@@ -67,7 +104,23 @@ const createCSV = function (dummyData) {
  * 
  ********************************************************************/
 const exportCSV = function (exportData) {
-    
+    fileOpenError = { 
+        errno: -4082,
+        code: 'EBUSY',
+        syscall: 'open'
+    }
+    fs.writeFile(outputLocation + outputName, exportData, function(err){
+        if (err) {
+            if (err.errno === -4082 || err.code === 'EBUSY') {
+                console.error('You need to close this file before you can overwrite its contents.')
+            } else{
+                console.error('Something Definitely Went Wrong: ');
+                console.error(err);
+            }
+        } else {
+            console.log('Successfully Output new file: ' + outputName);
+        }
+    });
 }
 
 
@@ -75,8 +128,9 @@ const exportCSV = function (exportData) {
  * 
  ********************************************************************/
 function main () {
+    checkUserVariables();
     const csvData = fs.readFileSync(dsv_location, 'utf-8');
-    console.log(csvData);
+    // console.log(csvData);
     var data = d3.csvParse(csvData);
     // console.log(data);
     var dummyData = conversionSeries(data);
