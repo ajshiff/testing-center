e057eb387aecbc140e43ad0430dc8002a950a2f8
ajshiff <ajshiffler@gmail.com>
2018-06-29 17:42:02 -0600

    removed debugging console.log statements and unused code

--- createDummyData_fromCSV.js
+++ createDummyData_fromCSV.js
@@ -9,7 +9,6 @@
 // DECLARE DEPENDENCIES AND CONSTANTS
 const fs = require('fs');
 const d3 = require('d3-dsv');
-// const dsv_location = 'C:\\Users\\ajshiff\\Documents\\git\\d3-countries-map\\src\\copy-of-countries.csv';
 const dsv_location = process.argv[2];
 var outputLocation = process.argv[3];
 var outputName = process.argv[4];
@@ -45,7 +44,7 @@ const checkUserVariables = function () {
     if(doNotContinue){process.exit(console.log(err));}
 
     // CHECK ARGUEMENT 3
-    if(!process.argv[4]) {outputName = 'new.csv'}
+    if(!outputName) {outputName = 'new.csv'}
     if(outputName.slice(-4) !== '.csv'){outputName += '.csv';}
     
 }
@@ -58,27 +57,22 @@ const checkUserVariables = function () {
  ********************************************************************/
 const conversionSeries = function (data) {
     var newData;
-    // let columnCounter = 0
     data.columns.forEach(column => {
         let rowCounter = 0;
         newData = data.map(value => {
             let returnValue = '';
-            // console.log(value[column]);
             if (value[column].toLowerCase() !== 'no data'){
                 returnValue = column + '_name_' + rowCounter;
             } else {
                 returnValue = value[column]+ '_' + column + '_' + rowCounter;
             }
             rowCounter += 1;
-            // console.log(newData);
             return returnValue;
         });
-        // columnCounter += 1;
         data.forEach((row, index) => {
             row[column] = newData[index];
         });
     });
-    // console.log(data);
     return data;
 }
 
@@ -97,7 +91,6 @@ const createCSV = function (dummyData) {
             });
         output = output.slice(0,-1);
     });
-    // console.log(output);
     return output;
 }
 
@@ -121,20 +114,15 @@ const exportCSV = function (exportData) {
     });
 }
 
-
 /********************************************************************
  * 
  ********************************************************************/
 function main () {
     checkUserVariables();
     const csvData = fs.readFileSync(dsv_location, 'utf-8');
-    // console.log(csvData);
     var data = d3.csvParse(csvData);
-    // console.log(data);
     var dummyData = conversionSeries(data);
-    // console.log(dummyData);
     var exportData = createCSV(dummyData);
-    // console.log(exportData);
     exportCSV(exportData);
 }
 
